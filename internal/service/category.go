56fecb6910d9ab2b89ca765cf2bcf628ff177519
Iann Isacksson <iisackssonoliveira@gmail.com>
2023-08-28 14:29:52 +0000

    trabalhando com stream

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/iannisacksson/gRPC/internal/database"
 	"github.com/iannisacksson/gRPC/internal/pb"
@@ -65,4 +66,32 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		// Se finalizar, fim do stream. Vai enviar
+		if err == io.EOF {
+			// Envia e fecha a conexão
+			return stream.SendAndClose(categories)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
